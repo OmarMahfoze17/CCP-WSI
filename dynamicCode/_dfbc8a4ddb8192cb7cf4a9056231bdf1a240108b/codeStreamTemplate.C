e350ecd5c2c2087a4689793e117f3b1b407c6975
OmarMahfoze17 <omar-ahmed.mahfoze@stfc.ac.uk>
2021-09-07 01:05:20 +0100

    The top right corner is moving witunder the effect of waves, spring and damper. PROBLEM: There is artefact force in x-direction

---
+++ dynamicCode/_dfbc8a4ddb8192cb7cf4a9056231bdf1a240108b/codeStreamTemplate.C
@@ -0,0 +1,78 @@
+/*---------------------------------------------------------------------------*  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) YEAR OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Template for use with codeStream.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dictionary.H"
+#include "Ostream.H"
+#include "Pstream.H"
+#include "unitConversion.H"
+
+//{{{ begin codeInclude
+#line 49 "/home/omar/OpenFOAM/omar-8/run/CCP_WSI/constant/dynamicMeshDict/sixDoFRigidBodyMotionCoeffs/#codeStream"
+#include "diagTensor.H"
+//}}} end codeInclude
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    void codeStream_dfbc8a4ddb8192cb7cf4a9056231bdf1a240108b
+    (
+        Ostream& os,
+        const dictionary& dict
+    )
+    {
+//{{{ begin code
+        #line 54 "/home/omar/OpenFOAM/omar-8/run/CCP_WSI/constant/dynamicMeshDict/sixDoFRigidBodyMotionCoeffs/#codeStream"
+scalar sqrLx = sqr(3.500000e-02);
+            scalar sqrLy = sqr(7.000000e-03);
+            scalar sqrLz = sqr(5.000000e-03);
+            os  <<
+                1.225000e-03
+               *diagTensor(sqrLy + sqrLz, sqrLx + sqrLz, sqrLx + sqrLy)/12.0;
+//}}} end code
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
